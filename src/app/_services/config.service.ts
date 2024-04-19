a9d63f7045c9b2e97b6d517fff0d5c366f1a6255
Adrià Martí Comas <adriamarticomas@gmail.com>
2024-04-19 11:30:13 +0200

    cors fixed

--- src/app/_services/config.service.ts
+++ src/app/_services/config.service.ts
@@ -8,8 +8,7 @@ export class ConfigService {
   private http = inject(HttpClient);
 
   sendEmail(emailBody: any) {
-    // return this.http.post('http://localhost:8080/apiAdri/enviar', emailBody);
-    //Solucions problemes cors
+
     return this.http.post('https://adriamarticomas.com/apiAdri/enviar', emailBody);
   }
 }
